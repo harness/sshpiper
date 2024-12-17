6c3c7a1bf0057b92ca823bb4b1cf6633a0820227
Abhinav Singh <abhinav.singh@harness.io>
2024-12-16 17:51:48 -0800

    feat: add remote call authn (#3)

---
+++ plugin/internal/remotecall/remotecall.go
@@ -0,0 +1,222 @@
+package remotecall
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+const (
+	UserAgent           = "User-Agent"
+	UserAgentSSHGateway = "SSH-Gateway"
+	Accept              = "Accept"
+	ContentType         = "Content-Type"
+	ApplicationJson     = "application/json"
+
+	AuthTokenUserClusterMapping = "authToken"
+	Authorization               = "Authorization"
+	IdentitySSHGateway          = "SSH-gateway"
+	JwtValidity                 = 5 * time.Second
+)
+
+type RemoteCall struct {
+	userClusterNameURL *url.URL
+	userClusterToken   string
+
+	clusterNameToAuthenticatorURL map[string]*url.URL
+	serviceJwtProvider            map[string]*ServiceJWTProvider
+
+	// keeping it string since these won't have http
+	clusterNameToUpstreamURL map[string]string
+
+	mappingKeyFile []byte
+
+	httpClient *http.Client
+}
+
+func InitRemoteCall(
+	userClusterNameURL string,
+	userClusterToken string,
+	clusterNameToAuthenticatorURL map[string]string,
+	serviceJwtToken map[string]string,
+	clusterNameToUpstreamURL map[string]string,
+	mappingKeyPath string,
+) (*RemoteCall, error) {
+	userClusterNameURLParsed, err := url.Parse(userClusterNameURL)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing userClusterNameURL %q: %w", userClusterNameURL, err)
+	}
+
+	clusterNameToAuthenticatorURLParsed := make(map[string]*url.URL, len(clusterNameToAuthenticatorURL))
+
+	for clusterName, clusterURL := range clusterNameToAuthenticatorURL {
+		clusterURLParsed, err := url.Parse(clusterURL)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing URL %q: %w", clusterURL, err)
+		}
+		clusterNameToAuthenticatorURLParsed[clusterName] = clusterURLParsed
+	}
+
+	jwtProviders := make(map[string]*ServiceJWTProvider, len(serviceJwtToken))
+	for clusterName, secret := range serviceJwtToken {
+		serviceJWTProvider, err := NewServiceJWTProvider(IdentitySSHGateway, []byte(secret), JwtValidity)
+		if err != nil {
+			return nil, fmt.Errorf("error creating jwt provider: %w", err)
+		}
+		jwtProviders[clusterName] = serviceJWTProvider
+	}
+
+	key, err := os.ReadFile(mappingKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading mapping key: %w", err)
+	}
+
+	return &RemoteCall{
+		userClusterNameURL:            userClusterNameURLParsed,
+		userClusterToken:              userClusterToken,
+		clusterNameToAuthenticatorURL: clusterNameToAuthenticatorURLParsed,
+		serviceJwtProvider:            jwtProviders,
+		httpClient:                    createHttpClient(),
+		mappingKeyFile:                key,
+		clusterNameToUpstreamURL:      clusterNameToUpstreamURL,
+	}, nil
+}
+
+func createHttpClient() *http.Client {
+	return &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			MaxIdleConns:    10,
+			IdleConnTimeout: 30 * time.Second,
+		},
+	}
+}
+
+func (r *RemoteCall) GetClusterName(username string) (string, error) {
+	if username == "" {
+		return "", fmt.Errorf("empty username")
+	}
+	req, err := http.NewRequest("GET", r.userClusterNameURL.JoinPath(username).String(), nil)
+	if err != nil {
+		return "", fmt.Errorf("error creating request for getting cluster name with url: %q and username:"+
+			" %q :%w", r.userClusterNameURL, username, err)
+	}
+
+	req.Header.Set(AuthTokenUserClusterMapping, r.userClusterToken)
+	userClusterResponse := UserClusterResponse{}
+	err = r.performHttpRequest(req, &userClusterResponse)
+	if err != nil {
+		return "", fmt.Errorf("error doing http call for GetClusterName: %w", err)
+	}
+	return userClusterResponse.ClusterName, nil
+}
+
+func (r *RemoteCall) performHttpRequest(req *http.Request, response any) error {
+	// Set custom headers if needed
+	req.Header.Set(UserAgent, UserAgentSSHGateway)
+	req.Header.Set(Accept, ApplicationJson)
+	req.Header.Set(ContentType, ApplicationJson)
+
+	resp, err := r.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error making request: %w", err)
+	}
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("error reading response body : %w", err)
+		}
+		return fmt.Errorf("error: status code for url: %s, error: %q: %d", req.URL.String(), bodyBytes,
+			resp.StatusCode)
+	}
+	err = json.NewDecoder(resp.Body).Decode(response)
+	if err != nil {
+		return fmt.Errorf("error reading response body for GetClusterName: %w", err)
+	}
+
+	return nil
+}
+
+// todo: refactor once we have user info in directory svc
+
+func (r *RemoteCall) AuthenticateKey(
+	key []byte,
+	keyType string,
+	clusterURL string,
+	clusterName string,
+	accountId string,
+) (*UserKeyAuthResponse, error) {
+	token, err := r.getUpstreamAuthenticatorAuthToken(clusterName)
+	if err != nil {
+		return nil, fmt.Errorf("error getting authenticator token from cluster name: %w", err)
+	}
+
+	sshKeyObj := sshKeyObject{
+		Key:       key,
+		Algorithm: keyType,
+	}
+	auth := userKeyAuthRequest{
+		SshKeyObject: sshKeyObj,
+		AccountId:    accountId,
+	}
+	body, err := json.Marshal(auth)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling auth: %v", auth)
+	}
+
+	req, err := http.NewRequest("POST", clusterURL, bytes.NewReader(body))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request for auth: %w", err)
+	}
+
+	req.Header.Set(Authorization, token)
+	authResponse := &UserKeyAuthResponse{}
+
+	err = r.performHttpRequest(req, &authResponse)
+	if err != nil {
+		return nil, fmt.Errorf("error performing http request for AuthenticateKey: %w", err)
+	}
+
+	return authResponse, nil
+}
+
+func (r *RemoteCall) MapKey() []byte {
+	return r.mappingKeyFile
+}
+
+func (r *RemoteCall) GetUpstreamAuthenticatorURL(clusterName string) (string, error) {
+	clusterURL, ok := r.clusterNameToAuthenticatorURL[clusterName]
+	if !ok {
+		return "", fmt.Errorf("unknown cluster %q", clusterName)
+	}
+	return clusterURL.String(), nil
+}
+
+func (r *RemoteCall) getUpstreamAuthenticatorAuthToken(clusterName string) (string, error) {
+	jwtProvider, ok := r.serviceJwtProvider[clusterName]
+	if !ok {
+		return "", fmt.Errorf("unknown cluster for jwt token %q", clusterName)
+	}
+	jwt, err := jwtProvider.GetJWT()
+	if err != nil {
+		return "", fmt.Errorf("error getting jwt token for cluster %q: %w", clusterName, err)
+	}
+	return jwt, nil
+}
+
+func (r *RemoteCall) GetUpstreamSvcURL(clusterName string) (string, error) {
+	clusterURL, ok := r.clusterNameToUpstreamURL[clusterName]
+	if !ok {
+		return "", fmt.Errorf("unknown upstream cluster %q", clusterName)
+	}
+	return clusterURL, nil
+}
---
+++ plugin/internal/remotecall/serviceJwtProvider.go
@@ -0,0 +1,103 @@
+package remotecall
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+const (
+	// defaultJWTValidFor specifies for how long a jwt token is valid.
+	defaultJWTValidFor = 1 * time.Second
+)
+
+type ServiceJWTProvider struct {
+	serviceIdentity string
+	signingKey      []byte
+	jwtValidFor     time.Duration
+
+	// cached token
+	currentJWT   string
+	refreshAfter time.Time
+	lock         sync.Mutex
+}
+
+func NewServiceJWTProvider(
+	serviceIdentity string,
+	secret []byte,
+	jwtValidFor time.Duration,
+) (*ServiceJWTProvider, error) {
+	if serviceIdentity == "" {
+		return nil, fmt.Errorf("serviceIdentity used to identify JWT tokens can't be empty")
+	}
+
+	if len(secret) == 0 {
+		return nil, fmt.Errorf("secret used to sign JWT tokens can't be empty")
+	}
+
+	if jwtValidFor <= 0 {
+		jwtValidFor = defaultJWTValidFor
+	}
+
+	return &ServiceJWTProvider{
+		serviceIdentity: serviceIdentity,
+		signingKey:      secret,
+		jwtValidFor:     jwtValidFor,
+	}, nil
+}
+
+func (p *ServiceJWTProvider) GetJWT() (string, error) {
+	now := time.Now().UTC()
+	// avoid lock for in case of fresh token
+	if now.Before(p.refreshAfter) {
+		return p.currentJWT, nil
+	}
+
+	// generate new token (in lock)
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	// check if token already got refreshed while we waited for lock
+	if now.Before(p.refreshAfter) {
+		return p.currentJWT, nil
+	}
+
+	expiresAt := now.Add(p.jwtValidFor)
+	newToken, err := NewServiceJWT(p.serviceIdentity, p.signingKey, expiresAt)
+	if err != nil {
+		return "", fmt.Errorf("failed to generat new token: %w", err)
+	}
+
+	p.currentJWT = newToken
+	p.refreshAfter = now.Add(p.jwtValidFor / 2) // refresh after half of life is reached
+
+	return p.currentJWT, nil
+}
+
+func (p *ServiceJWTProvider) Provide() (string, error) {
+	return p.GetJWT()
+}
+
+func NewServiceJWT(identity string, signingKey []byte, expiresAt time.Time) (string, error) {
+	now := time.Now()
+	claims := JWTClaims{
+		Type: PrincipalTypeService,
+		Name: identity,
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "Harness Inc",
+			IssuedAt:  now.Unix(),
+			ExpiresAt: expiresAt.Unix(),
+			NotBefore: now.Add(-time.Hour * 1).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", fmt.Errorf("error signing token: %w", err)
+	}
+
+	return fmt.Sprintf("%s %s", identity, signedToken), nil
+}
